505a73ad06b4775430cb272fa54184027447a638
matt-primrose <matt.c.primrose@intel.com>
2023-02-24 21:23:04 -0700

    fix: add rate limiting

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ import { ClassMetaData, Logger, LogType } from './common'
 import { MessageHandler, MessageObject, MessageRequest } from './messageHandler'
 import { DigestAuth } from './digestAuth'
 import { SocketHandler, SocketParameters } from './socketHandler'
+import ExpressBrute = require("express-brute")
 
+const store = new ExpressBrute.MemoryStore()
+const bruteForce = new ExpressBrute(store)
 export const logLevel = 'debug'
 const app = express()
 const serverPort = process.env.PORT ?? 3001
@@ -33,13 +36,13 @@ class HttpResponse {
 app.use(express.static('public'))
 app.use(bodyParser.json({ type: 'application/json' }))
 
-app.route('/').get((req, res) => {
+app.route('/').get(bruteForce.prevent, (req, res) => {
   res.sendFile('index.html')
 })
 
-app.route('/classes').get(async (req, res) => { res.status(200).send(ClassMetaData) })
+app.route('/classes').get(bruteForce.prevent, async (req, res) => { res.status(200).send(ClassMetaData) })
 
-app.route('/connect').post(async (req, res) => {
+app.route('/connect').post(bruteForce.prevent, async (req, res) => {
   const request: HttpRequest = req.body
   if (request.address == null || request.port == null || request.username == null || request.password == null) {
     res.status(404).json({ error: 'invalid request' })
@@ -58,7 +61,7 @@ app.route('/connect').post(async (req, res) => {
   }
 })
 
-app.route('/wsman').post(async (req, res) => {
+app.route('/wsman').post(bruteForce.prevent, async (req, res) => {
   if (req.body) {
     const messageHandler = new MessageHandler(socketHandler, digestAuth)
     const messageObject: any = messageHandler.createMessageObject(req.body)
@@ -76,7 +79,7 @@ app.route('/wsman').post(async (req, res) => {
   }
 })
 
-app.route('/submit').post(async (req, res) => {
+app.route('/submit').post(bruteForce.prevent, async (req, res) => {
   if (digestAuth == null || socketHandler == null) {
     res.status(500).send('Error: not connected')
     Logger(LogType.ERROR, 'INDEX', 'Error: not connected')
@@ -102,7 +105,7 @@ app.route('/submit').post(async (req, res) => {
   }
 })
 
-app.route('/disconnect').delete((req, res) => {
+app.route('/disconnect').delete(bruteForce.prevent, (req, res) => {
   if (socketHandler.socket !== null) {
     socketHandler.socket.destroy()
     socketHandler.socket = null
